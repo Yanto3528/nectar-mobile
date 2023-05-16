0770d7955e39d68dfb2d0c857115c0ebf8f249e2
Yanto Lee <yanto3528@gmail.com>
2023-05-16 19:51:33 +0700

    feat: added save button for expense screen

--- src/screens/Expense/Expense.tsx
+++ src/screens/Expense/Expense.tsx
@@ -1,35 +1,73 @@
 import { useState } from "react";
 
-import { View } from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
 
-import { CustomText, Input, DatePicker, RadioGroup } from "@/components/ui";
+import {
+  CustomText,
+  Input,
+  DatePicker,
+  RadioGroup,
+  Button,
+} from "@/components/ui";
 
 export default function Expense() {
   const [radioValue, setRadioValue] = useState("0");
 
   return (
-    <View className="flex-1 p-4 bg-light">
-      <CustomText className="font-inter-bold text-lg mb-2">
-        Total Expense
-      </CustomText>
-      <View style={{ gap: 16 }}>
-        <Input keyboardType="decimal-pad" placeholder="Enter amount" />
-        <DatePicker label="Date" />
-        <RadioGroup
-          orientation="horizontal"
-          value={radioValue}
-          onChange={setRadioValue}
-          label="Transaction status"
-        >
-          <RadioGroup.Radio value="0" bordered>
-            Settled
-          </RadioGroup.Radio>
-          <RadioGroup.Radio value="1" bordered>
-            Not settled yet
-          </RadioGroup.Radio>
-        </RadioGroup>
-        <Input placeholder="Enter the name of this transaction" label="Name" />
+    <SafeAreaView className="flex-1 py-4 bg-light relative">
+      <ScrollView className="flex-1 px-4 mb-14">
+        <CustomText className="font-inter-bold text-lg mb-2">
+          Total Expense
+        </CustomText>
+        <View style={{ gap: 16 }}>
+          <Input keyboardType="decimal-pad" placeholder="Enter amount" />
+          <DatePicker label="Date" />
+          <RadioGroup
+            orientation="horizontal"
+            value={radioValue}
+            onChange={setRadioValue}
+            label="Transaction status"
+          >
+            <RadioGroup.Radio value="0" bordered>
+              Settled
+            </RadioGroup.Radio>
+            <RadioGroup.Radio value="1" bordered>
+              Not settled yet
+            </RadioGroup.Radio>
+          </RadioGroup>
+          <Input
+            placeholder="Enter the name of this transaction"
+            label="Name"
+          />
+          <Input
+            placeholder="Enter the name of this transaction"
+            label="Name"
+          />
+          <Input
+            placeholder="Enter the name of this transaction"
+            label="Name"
+          />
+          <Input
+            placeholder="Enter the name of this transaction"
+            label="Name"
+          />
+          <Input
+            placeholder="Enter the name of this transaction"
+            label="Name"
+          />
+          <Input
+            placeholder="Enter the name of this transaction"
+            label="Name"
+          />
+          <Input
+            placeholder="Enter the name of this transaction"
+            label="Name"
+          />
+        </View>
+      </ScrollView>
+      <View className="absolute bottom-0 left-0 w-screen px-4 py-2 items-center">
+        <Button>Save</Button>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
